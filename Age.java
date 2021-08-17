a1e569dcbda8f9c31e8776167beb97ee0042ef96
David <88966704+nosdrahcirvsky@users.noreply.github.com>
2021-08-17 04:08:42 -0300

    Estudos com java.time.LocalDate e java.time.Period

---
+++ Age.java
@@ -0,0 +1,22 @@
+package com.estudos;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Age {
+    public static void main(String args[]) {
+        int month = 10;
+        int year = 1997;
+        int day = 11;
+        
+        LocalDate today = LocalDate.now();
+        LocalDate bday = LocalDate.of(year, month, day);
+        int age = Period.between(bday, today).getYears();
+
+        if (day == today.getDayOfMonth() && month == today.getMonthValue()) {
+            System.out.println("Parabéns! Você está fazendo " + age + " anos =)");
+        } else {
+            System.out.println("Sua idade: " + age);
+        }
+    }
+}
